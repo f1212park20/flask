07b77813b9b7ef7af2250d5fa09908b9275a54fb
park seung won <f1212park20@gmail.com>
2025-11-27 11:47:19 +0900

    DB_test commit

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
+def get_connection():
+    connection = pymysql.connect(
+        host='127.0.0.1',  # MySQL 컨테이너 포트로 연결
+        user='flaskuser',
+        password='flaskpass',
+        database='testdb',
+        port=3306
+    )
+    return connection
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    conn=get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users;")
+            users = cursor.fetchall()  # 결과 가져오기
+
+    finally:
+        conn.close()
+    return render_template("index.html", users=users)
+
